5248d5952fa13b1fd9a3db39639efd42b1809c73
Jose Rojas <jose23rojas@unitec.edu>
2017-02-10 15:02:49 -0600

    Jose Rojas - Cifrado de Cesar TERMINADO

--- lab4.cpp
+++ lab4.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
+void cifradoCesar(string);
+
 int main()
 {
 	return 0;
+}
+
+void cifradoCesar(string texto)
+{
+	char textoCifrado[texto.size()];
+	for(int i = 0; i < texto.size(); i++)
+	{
+		if(texto[i] + 13 > 122)
+			textoCifrado[i] = ((texto[i] + 13) - 122) + 97;
+		else
+			textoCifrado[i] = texto[i] + 13;
+	}
+	cout << textoCifrado << endl;
 }
